0368202839a87e536e932fb323cd99ab2a0eb81e
Pablo George <pablogeokar@hotmail.com>
2025-06-29 14:46:27 -0300

    selecionando as moedas

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carteira_cripto/models/moeda.dart';
 import 'package:flutter_carteira_cripto/repositories/moeda_repository.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPage extends StatelessWidget {
+class MoedasPage extends StatefulWidget {
   const MoedasPage({super.key});
 
   @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  List<Moeda> selecionadas = [];
+  final tabela = MoedaRepository.tabela;
+  NumberFormat real = NumberFormat.currency(
+    locale: 'pt_BR',
+    name: 'R\$',
+    decimalDigits: 2,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    final tabela = MoedaRepository.tabela;
     return Scaffold(
       appBar: AppBar(title: Text('Cripto Moedas')),
       body: ListView.separated(
         itemBuilder: (BuildContext context, int moeda) {
           return ListTile(
-            leading: Image.asset(tabela[moeda].icone),
-            title: Text(tabela[moeda].nome),
-            trailing: Text(tabela[moeda].preco.toStringAsFixed(2)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            leading: (selecionadas.contains(tabela[moeda]))
+                ? CircleAvatar(child: Icon(Icons.check))
+                : SizedBox(width: 40, child: Image.asset(tabela[moeda].icone)),
+            title: Text(
+              tabela[moeda].nome,
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+            ),
+            trailing: Text(real.format(tabela[moeda].preco)),
+            selectedTileColor: Colors.indigo[50],
+            selected: selecionadas.contains(tabela[moeda]),
+            onLongPress: () {
+              setState(() {
+                (selecionadas.contains(tabela[moeda]))
+                    ? selecionadas.remove(tabela[moeda])
+                    : selecionadas.add(tabela[moeda]);
+              });
+            },
           );
         },
         padding: EdgeInsets.all(16),
